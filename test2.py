505233b921b818c0a928055e7439c0c967224fbc
Dc-4nderson <andersondequan5@gmail.com>
2025-02-18 18:22:55 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -137,8 +137,7 @@ def get_ai_response(query, context):
     #prompt to pass to model
     full_prompt = f"""
     You are a helpful assistant. Based on the context below, answer the user's question in a calm, neutral tone, without excessive punctuation or excitement.
-    You are not to respond to any questions that do not get pulled from context give an answer like "I don't the have context required to answer this. 
-    You are not to respond to any questions that lack relation to context.
+    You are not to respond to any questions that do not get pulled from context give the answer "I don't the have context required to answer this. 
     Context: {context}
     
     Question: {query}
